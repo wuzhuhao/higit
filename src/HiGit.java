bfa5e93178f053105320eaa80744cbffd2bed36b
wzh97 <1376261775@qq.com>
2019-12-12 20:44:49 +0800

    1111

--- src/HiGit.java
+++ src/HiGit.java
@@ -2,5 +2,6 @@ public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit");
         System.out.println("sdf");
+        System.out.println("123123");
     }
 }
