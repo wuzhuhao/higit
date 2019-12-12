b1de4957a709977b6ff9e44c622af0b1e5a84c63
wzh97 <1376261775@qq.com>
2019-12-13 01:01:52 +0800

    添加操作？？？

--- src/HiGit.java
+++ src/HiGit.java
@@ -1,8 +1,8 @@
 public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit");
+        //添加的
         System.out.println("哈哈哈");
-        //对比commit
-        System.out.println("对比");
+
     }
 }
