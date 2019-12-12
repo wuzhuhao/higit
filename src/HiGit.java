9e3df0825f5682e20576de045446d9df46363623
wzh97 <1376261775@qq.com>
2019-12-13 00:45:48 +0800

    1111

--- src/HiGit.java
+++ src/HiGit.java
@@ -2,5 +2,7 @@ public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit");
         System.out.println("哈哈哈");
+        //对比commit
+        System.out.println("对比");
     }
 }
